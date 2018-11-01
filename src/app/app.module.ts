774cd7a69145cc68964e253de60256dc9850de98
mahdi <mahdihp.devsc@gmail.com>
2018-11-01 11:17:16 +0330

    Git Init.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user/userlist/userlist.component';
+import { UserDetailsComponent } from './user/userdetails/userdetails.component';
+import { HttpClientModule } from '@angular/common/http'; 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserListComponent,
+    UserDetailsComponent
   ],
   imports: [
+    MatButtonModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
